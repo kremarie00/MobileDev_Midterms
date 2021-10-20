39abff92786d93b5cbad220792b1bce6e725fb1f
Krea Marie Alangcas <krea.marie.alangcas@gmail.com>
2021-10-20 14:27:31 +0800

    Update LoginPage.dart

--- midterm_project/lib/pages/LoginPage.dart
+++ midterm_project/lib/pages/LoginPage.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Dashboard.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
